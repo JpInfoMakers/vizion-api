460b2e1089d56253f30db08b4fa6d6ca71053dca
Kayke Mendes <kayke-dev@outlook.com>
2025-09-11 10:31:53 -0300

    fix size image

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ bodyLimit: 10 * 1024 * 1024 })
+  );
   app.enableCors({
     origin: [
       'http://localhost:9002',
