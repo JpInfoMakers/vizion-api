7dcb753eb038cf5f5eb20981d6935be5912aec4a
Kayke Mendes <kayke-dev@outlook.com>
2025-09-11 09:28:47 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:9002',
+      'https://tradervizion.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3333);
   console.log('🚀 App rodando em http://localhost:3333');
 }
